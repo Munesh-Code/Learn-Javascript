5bc1f4088dd00412c5d07473c8cb8e615138c74c
Munesh Kumar Singh <muneshkumar02140@gmail.com>
2024-01-22 07:25:15 +0000

    Datatypes

---
+++ 01-Basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict" //Treats all JS Code as newer Version
+
+// alert(2+3); //We are using nodejs, not browser 
+
+console.log(2 
+    +
+    3) // Code Readiablity Should be High
+
+console.log("Ikshit");
+
+let name = "Ikshit"
+let age = 21
+let isLoggedIn = false
+
+// number => 2^53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => Unique
+
+// Object
+
+
+console.log(typeof  undefined) // undefined
+console.log(typeof null) //object
